e65c7702d094aba9968b27e0d7bffe9c88d9e95c
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-03-02 21:34:13 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -11,8 +11,8 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 52.15.235.202:1080
-socks5_username = mikrotik999
+socks5 = 143.198.171.112:1080 
+socks5_username = mikrotik999 
 socks5_password = Elibawnos
 END
 
